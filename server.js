0b0096f848d6d6b11b14e664d5b0471cbb0493ea
bhagatpremsagar92-source <bhagatpremsagar92@gmail.com>
2026-09-11 17:31:28 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok", service: "S2 Grow" });
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`S2 Grow running on port ${PORT}`);
+});
