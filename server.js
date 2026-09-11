cac6c08304e20907531597f9e70b7e37d985fd0c
bhagatpremsagar92-source <bhagatpremsagar92@gmail.com>
2026-09-12 02:19:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,52 @@
+
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const SITE_URL = "https://s2grow.onrender.com";
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve files from the project folder
-app.use(express.static(__dirname));
-
-// Google Search Console verification file
+// Google Search Console verification
 app.get("/googleec2d84ebf54fe011.html", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "googleec2d84ebf54fe011.html")
+  res.type("text/plain").send(
+    "google-site-verification: googleec2d84ebf54fe011.html"
   );
 });
 
-// Main website
+// Robots.txt
+app.get("/robots.txt", (req, res) => {
+  res.type("text/plain").send(
+`User-agent: *
+Allow: /
+
+Sitemap: ${SITE_URL}/sitemap.xml
+`
+  );
+});
+
+// Sitemap.xml
+app.get("/sitemap.xml", (req, res) => {
+  res.type("application/xml").send(
+`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+  <url>
+    <loc>${SITE_URL}/</loc>
+  </url>
+
+</urlset>`
+  );
+});
+
+// Serve website files
+app.use(express.static(__dirname));
+
+// Homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
